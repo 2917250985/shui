b8142fc5be28bf08076543567b00f278ae6d002a
2917250985@@qq.com <101497670+2917250985@users.noreply.github.com>
2022-05-14 14:49:21 +0800

    maccms

---
+++ maccms/runtime/temp/1793ca22ba95c3ecb59fcc286e6f4094.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:73:"E:\2038050325\maccms/application/admin\view\extend\urlsend\baidufast.html";i:1647767725;}*/ ?>
+<div class="layui-tab-item ">
+
+    <div class="layui-form-item">
+        <label class="layui-form-label">token：</label>
+        <div class="layui-input-inline  ">
+            <input type="text" class="layui-input" value="<?php echo $config['baidufast']['token']; ?>" placeholder="" name="urlsend[baidufast][token]">
+        </div>
+    </div>
+
+</div>
---
+++ maccms/runtime/temp/3e481ce63cfb44ab41c6a51727c9d215.php
@@ -0,0 +1,11 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:69:"E:\2038050325\maccms/application/admin\view\extend\urlsend\baidu.html";i:1647767725;}*/ ?>
+<div class="layui-tab-item ">
+
+    <div class="layui-form-item">
+        <label class="layui-form-label">token：</label>
+        <div class="layui-input-inline  ">
+            <input type="text" class="layui-input" value="<?php echo $config['baidu']['token']; ?>" placeholder="" name="urlsend[baidu][token]">
+        </div>
+    </div>
+
+</div>
